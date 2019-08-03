0c3e95caf0d9473d13c0a17e48e7bfb0ccc96d4e
Caio Lima <caio.pesquisa@hotmail.com>
2019-08-03 00:49:41 -0300

    Adicionar arquivos de projeto.

---
+++ adonet-clientes/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(adonet_clientes.Startup))]
+namespace adonet_clientes
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
